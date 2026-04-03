69cd3f7606a0849ff8a0204f94ca89bfe5fe61bd
jinwoong96 <wlsdnd921@gmail.com>
2026-04-03 22:14:15 +0900

    import 수정, api 경로 수정

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,7 +1,6 @@
 from .inbounds import Inbound
 from .inventorys import Inventory
 from .locations import Location
-from .orders import Order
 from .outbounds import Outbound
 from .products import Product
 from .users import User
--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,7 +1,6 @@
 from .inbound import InboundService
 from .inventory import InventoryService
 from .location import LocationService
-from .order import OrderService
 from .outbound import OutboundService
 from .product import ProductService
 from .user import UserService
