cdf29907ecafa6b4df6b9f609727c4c6f8a389c9
jh <ljh11442@gmail.com>
2026-04-05 22:00:21 +0900

    feat: 입고, 출고, 재고 CRUD 작성

--- backend/app/db/crud/inbound.py
+++ backend/app/db/crud/inbound.py
@@ -1,5 +1,28 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from app.db.models import Inbound
+from app.db.scheme.inbounds import InboundCreate, InboundUpdate
+
 
 class InboundCrud:
-    pass
+    @staticmethod
+    async def create(db:AsyncSession, inbound:InboundCreate) -> Inbound:
+        db_inbound=Inbound(**inbound.model_dump())
+        db.add(db_inbound)
+        await db.flush()
+        return db_inbound
+    
+    @staticmethod
+    async def get_by_id(db:AsyncSession, product_id:int | None = None,
+                                         location_id:int | None = None) -> list[Inbound]:
+        
+        semiresult = select(Inbound)
+
+        if product_id:
+            semiresult = semiresult.filter(Inbound.product_id == product_id)
+        if location_id:
+            semiresult = semiresult.filter(Inbound.location_id == location_id)
+
+        result = await db.execute(semiresult)
+
+        return result.scalars.all()
